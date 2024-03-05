13a3e5198b9d1080ab8d5fd1681dda69006a08e6
jeykob1017 <kyriecurry3011@gmail.com>
2024-03-05 16:08:50 +0800

    register-web

---
+++ register.js
@@ -0,0 +1,16 @@
+console.log("Hello World!")
+console.error("Error Happened")
+console.warn("Warning")
+let name = "Jacob",
+ lastname = "Aguhob", //string
+ age = 18; //string
+ isProf = true, //boolean
+ hobbies = ["watch movies", "play video games"]; //object
+ run = function () {
+    console.log("Im running.");
+ }; //function
+console.log(name, age, isProf, hobbies, run);
+console.log(typeof run);
+console.table (hobbies);
+console.log("My name is " + name + "" + lastname);
+console.log(`My name is ${name} ${lastname}`);
